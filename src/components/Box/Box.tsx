7ab089dda3d471e31523e4439eba797a68ca2bc9
hamiltonspjr <hamilton-souza@outlook.com.br>
2024-02-01 15:44:16 -0300

    feat(boxComponents): created Box and TouchableOpacityBox component

---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,34 @@
+import {
+  createBox,
+  border,
+  BorderProps,
+  layout,
+  LayoutProps,
+  spacing,
+  SpacingProps,
+  backgroundColor,
+  BackgroundColorProps,
+  createRestyleComponent,
+  spacingShorthand,
+  SpacingShorthandProps,
+} from '@shopify/restyle';
+import {Theme} from '../../theme/theme';
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
+
+export const Box = createBox<Theme>();
+export type BoxProps = React.ComponentProps<typeof Box>;
+
+export type TouchableOpacityBoxProps = SpacingProps<Theme> &
+  LayoutProps<Theme> &
+  BorderProps<Theme> &
+  BackgroundColorProps<Theme> &
+  SpacingShorthandProps<Theme> &
+  TouchableOpacityProps;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityBoxProps,
+  Theme
+>(
+  [spacing, layout, border, backgroundColor, spacingShorthand],
+  TouchableOpacity,
+);
