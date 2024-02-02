c30a5b611bea6672a81807bc661a06ced6466ba2
hamiltonspjr <hamilton-souza@outlook.com.br>
2024-02-02 16:16:23 -0300

    feat(button): created button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {TouchableOpacityBox, TouchableOpacityBoxProps} from '../Box/Box';
+import {Text} from '../Text/Text';
+import {ActivityIndicator} from '../ActivityIndicator/ActivityIndicator';
+import {buttonPresets} from './buttonPresets';
+
+export type ButtonPresets = 'primary' | 'outline';
+
+export interface ButtonProps extends TouchableOpacityBoxProps {
+  title: string;
+  preset?: ButtonPresets;
+  disabled?: boolean;
+  loading?: boolean;
+}
+
+export function Button({
+  title,
+  preset = 'primary',
+  disabled,
+  loading,
+  ...touchableOpacityBoxProps
+}: ButtonProps) {
+  const buttonPreset = buttonPresets[preset][disabled ? 'disabled' : 'default'];
+  return (
+    <TouchableOpacityBox
+      disabled={disabled || loading}
+      paddingHorizontal="s24"
+      height={60}
+      alignItems="center"
+      justifyContent="center"
+      borderRadius="s6"
+      {...touchableOpacityBoxProps}
+      {...buttonPreset.container}>
+      {loading && <ActivityIndicator color={buttonPreset.content} />}
+      <Text preset="paragraphLarge" bold color={buttonPreset.content}>
+        {title}
+      </Text>
+    </TouchableOpacityBox>
+  );
+}
