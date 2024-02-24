e9947bfba9b7bd1452eb4ca5c164b5649cb4e2c5
hamiltonspjr <hamilton-souza@outlook.com.br>
2024-02-24 11:39:59 -0300

    feat(textInput): created component

---
+++ src/components/TextInput/TextInput.tsx
@@ -0,0 +1,79 @@
+import React, {useRef} from 'react';
+import {
+  Pressable,
+  TextInput as RNTextInput,
+  TextInputProps as RNTextInputProps,
+  TextStyle,
+} from 'react-native';
+import {Box, BoxProps} from '../Box/Box';
+import {$fontFamily, Text, $fontSizes} from '../Text/Text';
+import {useAppTheme} from '../../hooks/useAppTheme';
+
+interface TextInputProps extends RNTextInputProps {
+  label: string;
+  RightComponent?: React.ReactElement;
+  boxProps?: BoxProps;
+  errorMessage?: string;
+}
+
+export function TextInput({
+  label,
+  RightComponent,
+  boxProps,
+  errorMessage,
+  ...rnTextInputProps
+}: TextInputProps) {
+  const {colors} = useAppTheme();
+
+  const inputRef = useRef<RNTextInput>(null);
+
+  function focusInput() {
+    inputRef.current?.focus();
+  }
+
+  const inputBoxStyle: BoxProps = {
+    borderWidth: errorMessage ? 2 : 1,
+    borderColor: errorMessage ? 'error' : 'gray1',
+    borderRadius: 's6',
+    paddingHorizontal: 's8',
+    paddingVertical: 's16',
+    flexDirection: 'row',
+  };
+
+  return (
+    <Box {...boxProps}>
+      <Pressable onPress={focusInput}>
+        <Text preset="paragraphMedium" color="backgroundContrast" mb="s4">
+          {label}
+        </Text>
+        <Box {...inputBoxStyle}>
+          <RNTextInput
+            autoCapitalize="none"
+            ref={inputRef}
+            placeholderTextColor={colors.gray3}
+            style={$textInputStyle}
+            {...rnTextInputProps}
+          />
+          {RightComponent && (
+            <Box justifyContent="center" ml="s16">
+              {RightComponent}
+            </Box>
+          )}
+        </Box>
+        {errorMessage && (
+          <Text preset="paragraphSmall" color="error" mt="s4" bold>
+            {errorMessage}
+          </Text>
+        )}
+      </Pressable>
+    </Box>
+  );
+}
+
+const $textInputStyle: TextStyle = {
+  padding: 0,
+  flexShrink: 1,
+  flexGrow: 1,
+  fontFamily: $fontFamily.regular,
+  ...$fontSizes.paragraphMedium,
+};
