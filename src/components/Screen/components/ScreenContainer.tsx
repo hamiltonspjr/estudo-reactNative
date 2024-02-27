d901d596f1416d687499320567cfd7cf9dff6e2f
hamiltonspjr <hamilton-souza@outlook.com.br>
2024-02-27 19:36:07 -0300

    feat(screen): created ScreenContainer component

---
+++ src/components/Screen/components/ScreenContainer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {ScrollView, View} from 'react-native';
+
+interface ScreenContainerProps {
+  children: React.ReactNode;
+  backgroundColor: string;
+}
+
+export function ScrollViewContainer({
+  children,
+  backgroundColor,
+}: ScreenContainerProps) {
+  return (
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      style={{backgroundColor, flex: 1}}>
+      {children}
+    </ScrollView>
+  );
+}
+
+export function ViewContainer({
+  children,
+  backgroundColor,
+}: ScreenContainerProps) {
+  return <View style={{backgroundColor, flex: 1}}>{children}</View>;
+}
