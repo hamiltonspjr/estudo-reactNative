8f8d6ca2326ff1a11b9dad23b5461963cd669252
hamiltonspjr <hamilton-souza@outlook.com.br>
2024-03-01 15:24:37 -0300

    feat(navigation): added function goBack to home

--- src/components/Screen/components/ScreenHeader.tsx
+++ src/components/Screen/components/ScreenHeader.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Box, TouchableOpacityBox} from '../../Box/Box';
 import {Icon} from '../../Icon/Icon';
 import {Text} from '../../Text/Text';
+import {useNavigation} from '@react-navigation/native';
 
 interface Props {
   canGoBack?: boolean;
@@ -11,6 +12,8 @@ interface Props {
 const ICON_SIZE = 20;
 
 export function ScreenHeader({canGoBack, title}: Props) {
+  const navigation = useNavigation();
+
   return (
     <Box
       flexDirection="row"
@@ -18,7 +21,10 @@ export function ScreenHeader({canGoBack, title}: Props) {
       alignItems="center"
       justifyContent="space-between">
       {canGoBack && (
-        <TouchableOpacityBox flexDirection="row" alignItems="center">
+        <TouchableOpacityBox
+          flexDirection="row"
+          alignItems="center"
+          onPress={navigation.goBack}>
           <Icon size={ICON_SIZE} name="arrowLeft" color="primary" />
           {!title && (
             <Text preset="paragraphLarge" ml="s8">
