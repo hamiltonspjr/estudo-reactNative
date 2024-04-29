0e4b2d23ba9842bf0c9103657ed18176b4652441
hamiltonspjr <hamilton-souza@outlook.com.br>
2024-04-29 14:00:07 -0300

    feat(import): added import of new screens

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,3 +2,9 @@ export * from './auth/ForgotPasswordScreen/ForgotPasswordScreen';
 export * from './auth/LoginScreen/LoginScreen';
 export * from './auth/SignUpScreen/SignUpScreen';
 export * from './auth/SuccessScreen/SucessScreen';
+
+export * from './app/HomeScreen/HomeScreen';
+export * from './app/ProfileScreen/ProfileScreen';
+export * from './app/QueriesScreen/QueriesScreen';
+export * from './app/SearchScreen/SearchScreen';
+export * from './app/SettingsScreen/SettingsScreen';
