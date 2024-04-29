15f36006aa98aab974d476f8af50dfd6241b4a33
hamiltonspjr <hamilton-souza@outlook.com.br>
2024-04-29 13:52:31 -0300

    feat(tabBar): created custom tabBar

---
+++ src/routes/AppTabBar.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+import {BottomTabBarProps} from '@react-navigation/bottom-tabs';
+
+import {Box, Icon, Text, TouchableOpacityBox} from '@components';
+import {useAppSafeArea} from '@hooks';
+
+import {AppTabParamList} from './AppTabNavigator';
+import {mapScreenToProps} from './mapScreenToProps';
+
+export function AppTabBar({state, descriptors, navigation}: BottomTabBarProps) {
+  const {bottom} = useAppSafeArea();
+
+  return (
+    <Box
+      flexDirection="row"
+      backgroundColor="background"
+      paddingTop="s12"
+      style={{paddingBottom: bottom}}>
+      {state.routes.map((route, index) => {
+        const {options} = descriptors[route.key];
+
+        const tabItem = mapScreenToProps[route.name as keyof AppTabParamList];
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key,
+          });
+        };
+
+        return (
+          <TouchableOpacityBox
+            key={route.key}
+            activeOpacity={1}
+            alignItems="center"
+            accessibilityRole="button"
+            accessibilityState={isFocused ? {selected: true} : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            style={{flex: 1}}>
+            <Icon
+              name={isFocused ? tabItem.icon.focused : tabItem.icon.unfocused}
+              color={isFocused ? 'primary' : 'backgroundContrast'}
+            />
+            <Text preset="paragraphSmall" bold mt="s4">
+              {tabItem.label}
+            </Text>
+          </TouchableOpacityBox>
+        );
+      })}
+    </Box>
+  );
+}
